1c45fb0a8546c3cbbb3514ca7ee80d0592bb6469
Arturo Hernández <arturoaureliohr@gmail.com>
2023-02-24 22:31:45 -0400

    TASK-165: Implement Auth0 Provider in entry point component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { NavigationStack } from './src/features/navigation';
+import { Auth0Provider } from 'react-native-auth0';
+import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from '@env';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <NavigationStack />
-    </NavigationContainer>
+    <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID}>
+      <NavigationContainer>
+        <NavigationStack />
+      </NavigationContainer>
+    </Auth0Provider>
   );
 };
 
