d1f4e1fdd44af2d19073cb8c94d30d30ef0170dc
Arturo Hernández <arturoaureliohr@gmail.com>
2023-02-24 23:09:37 -0400

    TASK-165: Add basic login screen example to test Auth0 hook

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,3 +1,46 @@
+import { View, Button, Text, StyleSheet } from 'react-native';
+import { useAuth0 } from 'react-native-auth0';
+
 export const HomeScreen = () => {
-  return <></>;
+  const { authorize, clearSession, user } = useAuth0();
+
+  const onLogin = async () => {
+    try {
+      console.log('tiro login');
+      await authorize({ scope: 'openid profile email' });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onLogout = async () => {
+    try {
+      await clearSession();
+    } catch (e) {
+      console.log('Log out cancelled');
+    }
+  };
+
+  const loggedIn = user !== undefined && user !== null;
+
+  return (
+    <View style={styles.container}>
+      {loggedIn && <Text>You are logged in as {user.name}</Text>}
+      {!loggedIn && <Text>You are not logged in</Text>}
+
+      <Button
+        onPress={loggedIn ? onLogout : onLogin}
+        title={loggedIn ? 'Log Out' : 'Log In'}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
