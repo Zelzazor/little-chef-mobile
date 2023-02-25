60c8f19fa10962330cdda29a32d22963d2e57b89
Arturo Hernández <arturoaureliohr@gmail.com>
2023-02-24 21:45:06 -0400

    TASK-165: Implement React Native Navigation Container with Navigation Stack component in entry point component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { NavigationStack } from './src/features/navigation';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your application!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NavigationStack />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default { App };
