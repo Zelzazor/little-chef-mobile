e081b18fbaf5a4f702e931553ff6fc46c8d6f07d
Arturo Hernández <arturoaureliohr@gmail.com>
2023-02-26 00:27:06 -0400

    TASK-172: Add query client provider

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { NavigationStack } from './src/features/navigation';
 import { AuthProvider } from './src/features/auth';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <NavigationStack />
-      </NavigationContainer>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <NavigationContainer>
+          <NavigationStack />
+        </NavigationContainer>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
