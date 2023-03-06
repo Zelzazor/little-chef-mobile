5ba8a66cfb927c68eccc7ecc362287e8d70c8951
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-05 22:40:22 -0800

    Format metro config and entry point files

--- metro.config.js
+++ metro.config.js
@@ -11,7 +11,7 @@ module.exports = {
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
-        resetCache: true
+        resetCache: true,
       },
     }),
   },
