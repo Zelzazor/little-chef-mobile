e75e55a1f3a1dd091c8271402c5be2a4f229cd58
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-05 22:39:56 -0800

    Remove problematic ESLint rules extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
-    'standard-with-typescript',
   ],
   overrides: [],
   parserOptions: {
