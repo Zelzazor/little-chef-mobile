ed1a217ee0c6af3fe3e64dfbd465323c3e0c6cb9
srdaviid <luis.david.08@hotmail.com>
2023-04-05 08:45:35 -0400

    A file example of UseSearchContext

--- src/screens/search/Otro.tsx
+++ src/screens/search/Otro.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@rneui/base';
 import { Text, View } from 'react-native';
+import { useSearchContext } from '../../features/search/context/SearchContext';
 
 interface OtroProps {
   count: number;
@@ -6,9 +8,24 @@ interface OtroProps {
 }
 
 export const Otro = () => {
+  const { ingredients, addToList, removeFromList } = useSearchContext();
+
+  console.log(ingredients);
   return (
     <View>
+      <Button
+        onPress={() => {
+          addToList({ id: ingredients.length + 1, name: 'Pimienta' });
+        }}
+        title="Add"
+      />
       <Text>hola</Text>
+      <Button
+        onPress={() => {
+          removeFromList(ingredients.length);
+        }}
+        title="Remove"
+      />
     </View>
   );
 };
