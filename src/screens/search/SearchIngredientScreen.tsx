8aa363207c6ad0be244c4aa503de8e6a1f596b12
Arturo Hernández <arturoaureliohr@gmail.com>
2023-05-01 14:33:43 -0400

    Add search bar and ingredient list to search by ingredient screen

--- src/screens/search/SearchIngredientScreen.tsx
+++ src/screens/search/SearchIngredientScreen.tsx
@@ -1,9 +1,21 @@
 import { SearchBar } from '@rneui/base';
 import { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import {
+  FlatList,
+  Image,
+  Platform,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { config } from '../../config/app.config';
 import { useIngredients } from '../../features/search/hooks/useIngredients';
+import { type Ingredient } from '../../features/search/types';
+import { Pagination } from '../../features/ui/components/Pagination';
 import { useDebounce } from '../../features/utility/hooks/useDebounce';
+
 export const SearchIngredientScreen = () => {
   const { useGetIngredients } = useIngredients();
   const [page, setPage] = useState(1);
@@ -33,6 +45,38 @@ export const SearchIngredientScreen = () => {
         onChangeText={(e) => {
           setSearch(e);
         }}
+      />
+      <FlatList
+        data={data?.ingredients}
+        renderItem={({ item: ingredient }) => (
+          <Pressable
+            style={{
+              padding: 30,
+              borderColor: '#CCCCCC',
+              borderBottomWidth: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+            onPress={() => {
+              console.log({ ingredient });
+            }}
+          >
+            <Image
+              source={{ uri: ingredient.imageUrl }}
+              style={{ width: '50%', aspectRatio: 1, borderRadius: 10 }}
+            />
+            <Text>{ingredient.name}</Text>
+          </Pressable>
+        )}
+        keyExtractor={(ingredient: Ingredient) => ingredient.id}
+        ListFooterComponent={() => (
+          <Pagination
+            pagination={data?.pagination}
+            prevPage={prevPage}
+            nextPage={nextPage}
+          />
+        )}
       />
     </View>
   );
