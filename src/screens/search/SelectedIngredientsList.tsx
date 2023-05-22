08435a50497f1d98df1bd0fe7a7fbf2a608277c7
Arturo Hernández <arturoaureliohr@gmail.com>
2023-05-21 22:07:03 -0400

    TASK-232: Implement removable ingredient list in SelectedIngredients

--- src/screens/search/SelectedIngredientsList.tsx
+++ src/screens/search/SelectedIngredientsList.tsx
@@ -1,14 +1,19 @@
 import { type StackScreenProps } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
+import { IngredientList } from '../../features/search/components/IngredientList';
+import { useIngredientSearchContext } from '../../features/search/context/IngredientSearchContext';
 import { type SearchStackParamList } from '../../features/search/types';
 
 export interface IngredientListProps
   extends StackScreenProps<SearchStackParamList, 'RecipeDetails'> {}
 
 export const SelectedIngredientsList = () => {
+  const { ingredients } = useIngredientSearchContext();
+
   return (
     <View>
       <Text> Selected Ingredients</Text>
+      <IngredientList data={ingredients} removable />
     </View>
   );
 };
