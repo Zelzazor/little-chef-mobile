f260426fa07a56ffd1f356da4d660b1f67652a96
Arturo Hernández <arturoaureliohr@gmail.com>
2023-05-29 19:45:21 -0400

    TASK-232: Add Ingredients Search floating button to selected ingredients screen

--- src/screens/search/SelectedIngredientsList.tsx
+++ src/screens/search/SelectedIngredientsList.tsx
@@ -1,6 +1,7 @@
 import { type StackScreenProps } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
 import { IngredientList } from '../../features/search/components/IngredientList';
+import { IngredientSearchButton } from '../../features/search/components/IngredientSearchButton';
 import { useIngredientSearchContext } from '../../features/search/context/IngredientSearchContext';
 import { type SearchStackParamList } from '../../features/search/types';
 
@@ -10,18 +11,21 @@ export interface IngredientListProps
 export const SelectedIngredientsList = () => {
   const { ingredients } = useIngredientSearchContext();
   return (
-    <View>
-      <View style={{ borderBottomWidth: 1, borderBottomColor: '#CCC' }}>
-        <Text
-          style={{
-            padding: 10,
-            fontSize: 32,
-          }}
-        >
-          Selected Ingredients
-        </Text>
+    <>
+      <View>
+        <View style={{ borderBottomWidth: 1, borderBottomColor: '#CCC' }}>
+          <Text
+            style={{
+              padding: 10,
+              fontSize: 32,
+            }}
+          >
+            Selected Ingredients
+          </Text>
+        </View>
+        <IngredientList data={ingredients} removable />
       </View>
-      <IngredientList data={ingredients} removable />
-    </View>
+      <IngredientSearchButton />
+    </>
   );
 };
