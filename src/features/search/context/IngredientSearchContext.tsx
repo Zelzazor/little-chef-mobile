b0ebe32a455f7dc6ff28ac9ffd7f2992be0456f5
Arturo Hernández <arturoaureliohr@gmail.com>
2023-06-19 19:15:45 -0400

    TASK-245: Add tag list to ingredient search context

--- src/features/search/context/IngredientSearchContext.tsx
+++ src/features/search/context/IngredientSearchContext.tsx
@@ -1,14 +1,17 @@
 import { createContext, useContext, useReducer } from 'react';
 import { type FCC } from '../../../config';
 import { initialState, searchReducer } from '../reducer/searchReducer';
-import { type Ingredient } from '../types';
+import { type Ingredient, type Tag } from '../types';
 
 interface IngredientSearchContextProps {
-  count: number;
   ingredients: Ingredient[];
   addIngredient: (payload: Ingredient) => void;
   removeIngredient: (payload: string) => void;
   clearIngredients: () => void;
+  addTag: (payload: Tag) => void;
+  removeTag: (payload: string) => void;
+  clearTags: () => void;
+  clearFilters: () => void;
 }
 
 const IngredientSearchContext = createContext<IngredientSearchContextProps>(
@@ -19,30 +22,59 @@ export const IngredientSearchProvider: FCC = ({ children }) => {
   const [state, dispatch] = useReducer(searchReducer, initialState);
   const addIngredient = (payload: Ingredient) => {
     dispatch({
-      type: 'ADD_TO_LIST',
+      type: 'ADD_TO_INGREDIENT_LIST',
       payload,
     });
   };
 
   const removeIngredient = (payload: string) => {
     dispatch({
-      type: 'REMOVE_FROM_LIST',
+      type: 'REMOVE_FROM_INGREDIENT_LIST',
       payload,
     });
   };
 
   const clearIngredients = () => {
     dispatch({
-      type: 'CLEAR_LIST',
+      type: 'CLEAR_INGREDIENT_LIST',
+    });
+  };
+
+  const addTag = (payload: Tag) => {
+    dispatch({
+      type: 'ADD_TO_TAG_LIST',
+      payload,
+    });
+  };
+
+  const removeTag = (payload: string) => {
+    dispatch({
+      type: 'REMOVE_FROM_TAG_LIST',
+      payload,
+    });
+  };
+
+  const clearTags = () => {
+    dispatch({
+      type: 'CLEAR_TAG_LIST',
+    });
+  };
+
+  const clearFilters = () => {
+    dispatch({
+      type: 'CLEAR_FILTERS',
     });
   };
 
   const payload: IngredientSearchContextProps = {
-    count: state.count,
     ingredients: state.ingredients,
     addIngredient,
     removeIngredient,
     clearIngredients,
+    addTag,
+    removeTag,
+    clearTags,
+    clearFilters,
   };
 
   return (
