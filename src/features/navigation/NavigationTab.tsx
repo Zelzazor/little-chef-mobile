9dc4705793403f6ade184db4b960822a1841023c
Arturo Hernández <arturoaureliohr@gmail.com>
2023-07-02 19:09:11 -0400

    TASK-247: Hide voting screen if user is banned

--- src/features/navigation/NavigationTab.tsx
+++ src/features/navigation/NavigationTab.tsx
@@ -7,6 +7,7 @@ import { ProfileScreen } from '../../screens/profile/ProfileScreen';
 import { ReviewScreen } from '../../screens/review/ReviewScreen';
 import { SearchStackNavigation } from '../../screens/search/SearchStackNavigation';
 import { useAuthContext } from '../auth/context/useAuthContext';
+import { useUserContext } from '../user/context/useUserContext';
 import { SelectIcon } from './components/SelectIcon';
 import { type TabParamList } from './types';
 
@@ -16,6 +17,7 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 export const NavigationTab: FC = () => {
   const { loggedIn } = useAuthContext();
+  const { user } = useUserContext();
 
   return (
     <Tab.Navigator
@@ -37,7 +39,9 @@ export const NavigationTab: FC = () => {
       })}
     >
       <Tab.Screen name="SearchIndex" component={SearchStackNavigation} />
-      {loggedIn && <Tab.Screen name="Review" component={ReviewScreen} />}
+      {loggedIn && !user?.bannedAt && (
+        <Tab.Screen name="Review" component={ReviewScreen} />
+      )}
       {loggedIn && <Tab.Screen name="Profile" component={ProfileScreen} />}
       {!loggedIn && <Tab.Screen name="Login" component={LoginScreen} />}
     </Tab.Navigator>
